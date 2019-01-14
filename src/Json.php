5eb9c30a4b01213e7eb49c448eb065b9c6052326
Bob den Otter <bob@twokings.nl>
2019-01-14 17:39:12 +0100

    Working on ECS

--- src/Json.php
+++ src/Json.php
@@ -33,6 +33,54 @@ final class Json
     public const HTML = 15;
 
     /**
+     * Wrapper for json_decode that throws when an error occurs.
+     *
+     * @param string $json    JSON data to parse
+     * @param bool   $assoc   When true, returned objects will be converted
+     *                        into associative arrays.
+     * @param int    $depth   User specified recursion depth.
+     * @param int    $options Bitmask of JSON decode options.
+     *
+     * @throws \InvalidArgumentException if the JSON cannot be decoded.
+     *
+     * @see http://www.php.net/manual/en/function.json-decode.php
+     */
+    public static function json_decode($json, $assoc = false, $depth = 512, $options = 0)
+    {
+        $data = \json_decode($json, $assoc, $depth, $options);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException(
+                'json_decode error: ' . json_last_error_msg()
+            );
+        }
+
+        return $data;
+    }
+
+    /**
+     * Wrapper for JSON encoding that throws when an error occurs.
+     *
+     * @param mixed $value   The value being encoded
+     * @param int   $options JSON encode option bitmask
+     * @param int   $depth   Set the maximum depth. Must be greater than zero.
+     *
+     * @throws \InvalidArgumentException if the JSON cannot be encoded.
+     *
+     * @see http://www.php.net/manual/en/function.json-encode.php
+     */
+    public static function json_encode($value, $options = 0, $depth = 512): string
+    {
+        $json = \json_encode($value, $options, $depth);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException(
+                'json_encode error: ' . json_last_error_msg()
+            );
+        }
+
+        return $json;
+    }
+
+    /**
      * Dumps a array/object into a JSON string.
      *
      * @param mixed $data    Data to encode into a formatted JSON string
@@ -70,13 +118,12 @@ final class Json
     /**
      * Parses JSON into a PHP array.
      *
-     * @param string $json    The JSON string
-     * @param int    $options Bitmask of JSON decode options
-     * @param int    $depth   Recursion depth
+     * @param int $options Bitmask of JSON decode options
+     * @param int $depth   Recursion depth
      *
      * @throws ParseException If the JSON is not valid
      */
-    public static function parse($json, $options = 0, $depth = 512): array
+    public static function parse(?string $json = null, int $options = 0, int $depth = 512): ?array
     {
         if ($json === null) {
             return null;
--- src/Str.php
+++ src/Str.php
@@ -4,64 +4,58 @@ declare(strict_types=1);
 
 namespace Bolt\Common;
 
+use Cocur\Slugify\Slugify;
+
 class Str
 {
     /**
-     * Replaces the first occurrence of the $search text on the $subject.
+     * We use Slugify as a Singleton because Slugify::create() is quite heavy
      *
-     * @param string $subject
-     * @param string $search
-     * @param string $replace
-     * @param bool   $caseSensitive
+     * @var Slugify
      */
-    public static function replaceFirst($subject, $search, $replace, $caseSensitive = true): string
+    private static $slugifyInstance = null;
+
+    /** @var Slugify[] */
+    private static $slugifySafeInstances = [];
+
+    /**
+     * Replaces the first occurrence of the $search text on the $subject.
+     */
+    public static function replaceFirst(string $subject, string $search, string $replace, bool $caseSensitive = true): string
     {
         $pos = $caseSensitive ? mb_strpos($subject, $search) : mb_stripos($subject, $search);
         if ($pos === false) {
             return $subject;
         }
 
-        return substr_replace($subject, $replace, $pos, \mb_strlen($search));
+        return substr_replace($subject, $replace, $pos, mb_strlen($search));
     }
 
     /**
      * Replaces the last occurrence of the $search text on the $subject.
-     *
-     * @param string $subject
-     * @param string $search
-     * @param string $replace
-     * @param bool   $caseSensitive
      */
-    public static function replaceLast($subject, $search, $replace, $caseSensitive = true): string
+    public static function replaceLast(string $subject, string $search, string $replace, bool $caseSensitive = true): string
     {
         $pos = $caseSensitive ? mb_strrpos($subject, $search) : mb_strripos($subject, $search);
         if ($pos === false) {
             return $subject;
         }
 
-        return substr_replace($subject, $replace, $pos, \mb_strlen($search));
+        return substr_replace($subject, $replace, $pos, mb_strlen($search));
     }
 
     /**
      * Removes the first occurrence of the $search text on the $subject.
-     *
-     * @param string $subject
-     * @param string $search
-     * @param bool   $caseSensitive
      */
-    public static function removeFirst($subject, $search, $caseSensitive = true): string
+    public static function removeFirst(string $subject, string $search, bool $caseSensitive = true): string
     {
         return static::replaceFirst($subject, $search, '', $caseSensitive);
     }
 
     /**
      * Removes the last occurrence of the $search text on the $subject.
-     *
-     * @param string $subject
-     * @param string $search
-     * @param bool   $caseSensitive
      */
-    public static function removeLast($subject, $search, $caseSensitive = true): string
+    public static function removeLast(string $subject, string $search, bool $caseSensitive = true): string
     {
         return static::replaceLast($subject, $search, '', $caseSensitive);
     }
@@ -75,7 +69,7 @@ class Str
      *
      * @throws \InvalidArgumentException
      */
-    public static function splitFirst($subject, $delimiter): string
+    public static function splitFirst(string $subject, string $delimiter): string
     {
         Assert::notEmpty($delimiter);
 
@@ -93,7 +87,7 @@ class Str
      *
      * @throws \InvalidArgumentException
      */
-    public static function splitLast($subject, $delimiter): string
+    public static function splitLast(string $subject, string $delimiter): string
     {
         Assert::notEmpty($delimiter);
 
@@ -104,19 +98,15 @@ class Str
 
     /**
      * Returns whether the subjects ends with the search string.
-     *
-     * @param string $subject
-     * @param string $search
-     * @param bool   $caseSensitive
      */
-    public static function endsWith($subject, $search, $caseSensitive = true): bool
+    public static function endsWith(string $subject, string $search, bool $caseSensitive = true): bool
     {
         if (! $caseSensitive) {
             $subject = mb_strtolower($subject);
             $search = mb_strtolower($search);
         }
 
-        return $search === '' || mb_substr($subject, -\mb_strlen($search)) === $search;
+        return $search === '' || mb_substr($subject, -mb_strlen($search)) === $search;
     }
 
     /**
@@ -126,8 +116,8 @@ class Str
      */
     public static function className($class): string
     {
-        if (\is_object($class)) {
-            $class = \get_class($class);
+        if (is_object($class)) {
+            $class = get_class($class);
         }
 
         return static::splitLast($class, '\\');
@@ -144,7 +134,7 @@ class Str
      *
      * @return string The humanized text
      */
-    public static function humanize($text): string
+    public static function humanize(string $text): string
     {
         return ucfirst(trim(mb_strtolower(preg_replace(['/([A-Z])/', '/[_\s]+/'], ['_$1', ' '], $text))));
     }
@@ -157,7 +147,7 @@ class Str
      *
      * @return string The camel cased text
      */
-    public static function camelCase($text, $lowercaseFirstChar = false): string
+    public static function camelCase(string $text, bool $lowercaseFirstChar = false): string
     {
         $text = strtr(ucwords(strtr($text, [
             '_' => ' ',
@@ -178,10 +168,105 @@ class Str
      *
      * @return string The snake cased text
      */
-    public static function snakeCase($text): string
+    public static function snakeCase(string $text): string
     {
         return mb_strtolower(
             preg_replace(['/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'], ['\\1_\\2', '\\1_\\2'], $text)
         );
+    }
+
+    /**
+     * Returns a "safe" version of the given string - basically only US-ASCII and
+     * numbers. Needed because filenames and titles and such, can't use all characters.
+     */
+    public static function makeSafe(string $str, bool $strict = false, string $extrachars = ''): string
+    {
+        $str = str_replace('&amp;', '', $str);
+
+        $slugify = self::getSafeSlugify($strict, $extrachars);
+        $str = $slugify->slugify($str, '');
+
+        if ($strict) {
+            $str = str_replace(' ', '-', $str);
+        }
+
+        return $str;
+    }
+
+    public static function slug(string $str, $options = null): string
+    {
+        return self::getSlugify()->slugify($str, $options);
+    }
+
+    /**
+     * Add 'soft hyphens' &shy; to a string, so that it won't break layout in HTML when
+     * using strings without spaces or dashes. Only breaks in long (> 19 chars) words.
+     */
+    public static function shyphenate(string $str): string
+    {
+        $res = preg_match_all('/([a-z0-9]{19,})/i', $str, $matches);
+
+        if ($res) {
+            foreach ($matches[1] as $match) {
+                $str = str_replace($match, wordwrap($match, 10, '&shy;', true), $str);
+            }
+        }
+
+        return $str;
+    }
+
+    private static function getSlugify(): Slugify
+    {
+        if (self::$slugifyInstance === null) {
+            self::$slugifyInstance = Slugify::create();
+        }
+        return self::$slugifyInstance;
+    }
+
+    private static function getSafeSlugify(bool $strict = false, string $extrachars = ''): Slugify
+    {
+        $key = $strict ? 'strict_' : '' . $extrachars;
+
+        if (empty(self::$slugifySafeInstances[$key]) === true) {
+            $delim = '/';
+            if ($extrachars !== '') {
+                $extrachars = preg_quote($extrachars, $delim);
+            }
+            if ($strict) {
+                $slugify = Slugify::create([
+                    'regexp' => '/[^a-z0-9_' . $extrachars . ' -]+/',
+                ]);
+            } else {
+                // Allow Uppercase and don't convert spaces to dashes
+                $slugify = Slugify::create([
+                    'regexp' => '/[^a-zA-Z0-9_.,' . $extrachars . ' -]+/',
+                    'lowercase' => false,
+                ]);
+            }
+
+            self::$slugifySafeInstances[$key] = $slugify;
+        }
+
+        return self::$slugifySafeInstances[$key];
+    }
+
+    public static function generatePassword($length = 12)
+    {
+        // The "pool" of potential characters contains special characters, but
+        // with less frequency than 'a-z' and '0-9'.
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' .
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' .
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' .
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' .
+            '-=~!@#$%^&*()_+,./<>?;:[]{}\|';
+
+        $str = '';
+        $max = mb_strlen($chars) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $chars[random_int(0, $max)];
+        }
+
+        return $str;
     }
 }
